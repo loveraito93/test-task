d509d8fe1dc28d3fdcf326b6032ebfba2588bf54
Николай <136621176+loveraito93@users.noreply.github.com>
2023-06-15 12:28:04 +0600

    deploy

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,9 @@
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
+  devServer: {
+    allowedHosts: "all"
+  },
   transpileDependencies: true,
+  publicPath: process.env.NODE_ENV === "production" ? "/test-task/" : "/",
   outputDir: "docs"
 });
